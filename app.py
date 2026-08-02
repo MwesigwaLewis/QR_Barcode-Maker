9467b2f356157dfa4c5466df15a333f4a6255a95
Mwesigwa Lewis <mwesigwalewis15@gmail.com>
2026-08-02 16:27:40 +0300

    Fix main guard condition in app.py

--- app.py
+++ app.py
@@ -70,7 +70,7 @@ def download():
         download_name="Your_Code.png"
      )
 
-if __name__=="main":
+if __name__=="__main__":
       app.run(debug=True,
               host='0.0.0.0',
               port='5600'
